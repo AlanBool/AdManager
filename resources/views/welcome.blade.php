558b190c1177df06d1794e29eac1db47aa4f9a67
yingjun <yingjun@yingjundeMacBook-Air.local>
2018-01-25 01:31:46 +0800

    -mmodified home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel success 2
+                    Welcome
                 </div>
             </div>
         </div>
